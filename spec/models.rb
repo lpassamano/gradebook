03ff6060ec0a66c8fc4f7255e8b572aa7b68ec31
Leigh Passamano <leighayna.josele@gmail.com>
2017-09-29 00:56:19 +0000

    added test for Assessment

--- spec/models.rb
+++ spec/models.rb
@@ -61,3 +61,15 @@ describe Student do
   #has many courses
   #has many assessments
 end
+
+describe Assessment do
+  before do
+    @assessment = Assessment.create(name: "Final Exam", grade: "95", comment: "great job!")
+  end
+
+  it 'is instantiated with a name, grade, and comment' do
+    expect(@assessment.name).to eq("Final Exam")
+    expect(@assessment.grade).to eq("95")
+    expect(@assessment.comment).to eq("great job!")
+  end
+end
