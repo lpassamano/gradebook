b226bfc619697278837e31c141869f424cc9687d
Leigh Passamano <leighayna.josele@gmail.com>
2017-09-29 20:32:08 +0000

    added tests for signup

--- spec/03_controllers_spec.rb
+++ spec/03_controllers_spec.rb
@@ -8,4 +8,21 @@ describe ApplicationController do
       expect(last_response.body).to include("Welcome!")
     end
   end
+
+  describe "Signup" do
+    it 'loads the signup page' do
+      get '/signup' do
+        expect(last_response.status).to eq(200)
+      end
+    end
+
+    it 'does not let a user sign up without a name, email, and password' do
+      #check params for name, email, password 
+    end
+
+    it 'requires that you use your school/university email to signup' do
+      #use regex to make sure email ends with .edu
+    end
+  end
+
 end
