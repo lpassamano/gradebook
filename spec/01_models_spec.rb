858c6ba7bed56b8b66d407791e5482c123f4dfe3
Leigh Passamano <leighayna.josele@gmail.com>
2017-10-01 19:21:52 +0000

    deleted unneccessary studend and instructor tests

--- spec/01_models_spec.rb
+++ spec/01_models_spec.rb
@@ -25,30 +25,6 @@ describe User do
   end
 end
 
-describe Instructor do
-  before do
-    @instructor = Instructor.create(name: "test", email: "test@test.edu", password: "1234")
-  end
-
-  it 'is instantiated with a name' do
-    expect(@instructor.name).to eq("test")
-  end
-
-  it 'is instantiated with a email' do
-    expect(@instructor.email).to eq("test@test.edu")
-  end
-
-  it 'is instantiated with a password' do
-    expect(@instructor.password).to eq("1234")
-  end
-
-  it 'has a secure password' do
-    expect(@instructor.authenticate("test")).to eq(false)
-    expect(@instructor.authenticate("1234")).to eq(@instructor)
-  end
-  # slugifiable?
-end
-
 describe Course do
   before do
     @course = Course.create(name: "Test Class")
@@ -56,24 +32,6 @@ describe Course do
 
   it 'is instantiated with a name' do
     expect(@course.name).to eq("Test Class")
-  end
-  #slugifiable
-end
-
-describe Student do
-  before do
-    @student = Student.create(name: "Test Testerson", email: "student@test.edu", password: "asdf")
-  end
-
-  it 'is instantiated with a name, email, and password' do
-    expect(@student.name).to eq("Test Testerson")
-    expect(@student.email).to eq("student@test.edu")
-    expect(@student.password).to eq("asdf")
-  end
-
-  it 'has a secure password' do
-    expect(@student.authenticate("test")).to eq(false)
-    expect(@student.authenticate("asdf")).to eq(@student)
   end
   #slugifiable
 end
