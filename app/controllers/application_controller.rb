13721e350746a6da7b9d6edf5a2d37132b3f69be
Leigh Passamano <leighayna.josele@gmail.com>
2017-10-03 01:39:19 +0000

    removed helper methods that were replaced by User instance methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,13 +19,5 @@ class ApplicationController < Sinatra::Base
     def current_user
       User.find_by(id: session[:user_id])
     end
-
-    def current_user_student?
-      current_user.role == Role.find_by(name: "Student")
-    end
-
-    def current_user_instructor?
-      current_user.role == Role.find_by(name: "Instructor")
-    end
   end
 end
