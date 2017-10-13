46167f1b4afa52ab1b827cd7c4dfc431fe19c8bf
lpassamano <leighayna.josele@gmail.com>
2017-10-13 08:51:02 -0400

    added class methods #student_id and #instructor_id to Role class

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,11 @@
 class Role < ActiveRecord::Base
   has_many :users
+
+  def self.student_id
+    @@student_id ||= self.find_by(name: "Student").id
+  end
+
+  def self.instructor_id
+    @@instructor_id ||= self.find_by(name: "Instructor").id
+  end
 end
