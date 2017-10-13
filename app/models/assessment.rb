30c80448cb67b7e353e1f26513565e75b2e7aced
lpassamano <leighayna.josele@gmail.com>
2017-10-13 15:57:54 -0400

    cleaned up code

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -2,7 +2,4 @@ class Assessment < ActiveRecord::Base
   belongs_to :course
   has_many :grades
   has_many :users, through: :grades
-
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
 end
--- notes.md
+++
@@ -1,5 +0,0 @@
-To do:
-
-3. format table better in instructor view of course
-
-4. fix tests to all pass
